731d0fb45f5f9d70dc9b7a50e2c8bc0c55560c44
Elio Ervin <elio_ervin@icloud.com>
2020-12-14 08:54:25 -0800

    api fix

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/elioervin/bookmark;
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/elioervin/bookmark';
 
 /**
  * listApiFetch - Wrapper function for native `fetch` to standardize error handling. 
@@ -10,48 +10,48 @@ const BASE_URL = 'https://thinkful-list-api.herokuapp.com/elioervin/bookmark;
  */
 
 const listApiFetch = function (...args) {
-    let error;
-    return fetch(...args)
+  let error;
+  return fetch(...args)
     .then(res => {
-        if (!res.ok) {
-            error = { code: res.status };
+      if (!res.ok) {
+        error = { code: res.status };
 
-            if(!res.headers.get('content-type').includes('json')){
-                error.message = res.statusText
-            }
+        if(!res.headers.get('content-type').includes('json')){
+          error.message = res.statusText;
         }
-        return res.json();
+      }
+      return res.json();
     })
     .then(data => {
-        if (error) {
-            error.message = data.message;
-            return Promise.reject(error);
-        }
-        return data;
+      if (error) {
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+      return data;
     });
 };
 
 const getBookmarks = function () {
-    return listApiFetch(`${BASE_URL}/bookmarks`)
+  return listApiFetch(`${BASE_URL}/bookmarks`);
 };
 
 const createBookmark = function (newBookmark) {
-    return listApiFetch(`${BASE_URL}/bookmarks`,
+  return listApiFetch(`${BASE_URL}/bookmarks`,
     {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: newBookmark
-    })
-};
-
-const deleteBookmark = function (id) {
-    return listApiFetch(BASE_URL + '/bookmarks/' + id, {
-        method: 'DELETE'
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: newBookmark
     });
 };
 
+const deleteBookmark = function (id) {
+  return listApiFetch(BASE_URL + '/bookmarks/' + id, {
+    method: 'DELETE'
+  });
+};
+
 export default {
-    getBookmarks,
-    createBookmark,
-    deleteBookmark
-}
+  getBookmarks,
+  createBookmark,
+  deleteBookmark
+};
